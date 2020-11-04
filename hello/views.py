043e60464f1ce33c616a84833363c0bf611fb759
ByihtaAtJuni <hugo.miranda@junitec.pt>
2020-11-04 18:24:37 +0000

    Include urls from app

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
-# Create your views here.
+
+
+
+def index(request):
+	times = int(os.environ.get('TIMES',3))
+	return HttpResponse('Hello' * times)
+	
+def myView(request):
+	return HttpResponse("Hello, World Ahhhhh!")
+	
